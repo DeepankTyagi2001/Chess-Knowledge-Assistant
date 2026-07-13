7af380fc07f5367fa4b62a64a0949e4e2d71d6ff
DeepankTyagi2001 <deepanktyagi2001@gmail.com>
2026-07-13 13:19:49 +0530

    Update queries list

--- main.py
+++ main.py
@@ -11,32 +11,33 @@ from call_llm import call_llm
 FAISS_INDEX_PATH = "faiss_index_chess"
 
 def main():
-  query= input("Enter Your Query:\n")
-  embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
-  if os.path.exists(FAISS_INDEX_PATH):
-      print("Loading existing FAISS index from disk...")
-      vectorstore = FAISS.load_local(
-          FAISS_INDEX_PATH,
-          embeddings,
-          allow_dangerous_deserialization=True
-      )
-  else:
-      print("No existing index found. Building new FAISS index...")
+    
+    query= input("Enter Your Query:\n")
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+    if os.path.exists(FAISS_INDEX_PATH):
+        print("Loading existing FAISS index from disk...")
+        vectorstore = FAISS.load_local(
+            FAISS_INDEX_PATH,
+            embeddings,
+            allow_dangerous_deserialization=True
+        )
+    else:
+        print("No existing index found. Building new FAISS index...")
 
-      all_docs = load_data()            # got page wise chunked data
+        all_docs = load_data()            # got page wise chunked data
 
-      langchain_docs = chunker(all_docs)  # got actual chunked data
+        langchain_docs = chunker(all_docs)  # got actual chunked data
 
-      vectorstore = embedder(langchain_docs)
+        vectorstore = embedder(langchain_docs)
 
-      # Save locally so you don't have to re-embed every time
-      vectorstore.save_local("faiss_index_chess")
+        # Save locally so you don't have to re-embed every time
+        vectorstore.save_local("faiss_index_chess")
 
-      print(f"Saved new index to {FAISS_INDEX_PATH}")
+        print(f"Saved new index to {FAISS_INDEX_PATH}")
 
-  context = query_search(vectorstore, query)
+    context = query_search(vectorstore, query)
 
-  print(call_llm(query, context))
+    print(call_llm(query, context))
 
 
 if __name__=="__main__":
